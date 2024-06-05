5c69365741f930e4a25a12fbfaf88dd3f3d686df
Dakota M <PapaBeans@users.noreply.github.com>
2024-06-05 14:09:55 -0400

    Renamed Form1 to ParseMeForm

--- csharp/DocScraper/ParseMe/ParseMe/ParseMeForm.cs
+++ csharp/DocScraper/ParseMe/ParseMe/ParseMeForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ParseMe
 {
-    public partial class Form1 : Form
+    public partial class ParseMeForm : Form
     {
-        public Form1()
+        public ParseMeForm()
         {
             InitializeComponent();
         }
